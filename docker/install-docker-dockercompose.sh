8402a65e556fc7649a346fc35ff8bdb8edc5516c
htrungngx <delta199502@gmail.com>
2024-08-22 22:38:23 +0300

    Clean scripts

--- docker/install-docker-dockercompose.sh
+++ docker/install-docker-dockercompose.sh
@@ -5,10 +5,19 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install -y docker-ce
+sudo apt install -y docker-ce \
+                    docker-cli \ 
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker --version
 docker-compose --version
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+sudo sysctl -w vm.max_map_count=262144
+
+exit
