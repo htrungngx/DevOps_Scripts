c9453c975dccc0e8be1f636515c24fb76bbcb516
htrungngx <delta199502@gmail.com>
2024-08-28 14:47:49 +0300

    re-write(dockerscript)

--- docker/install-docker-dockercompose.sh
+++ docker/install-docker-dockercompose.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
 sudo apt update
-
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt update
-
-sudo apt install -y docker-ce
-
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install docker-ce -y
 sudo systemctl start docker
 sudo systemctl enable docker
-
-sudo usermod -aG docker $USER
-
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-docker --version
-
-docker-compose --version
+docker -v
+docker-compose -v
 
 sudo groupadd docker
 
